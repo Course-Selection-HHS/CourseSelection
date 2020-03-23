7d54e1e44d6855ce505866fab21ebf477fcd4668
Hamziniii <hamzagaziuddin04@gmail.com>
2020-03-23 13:56:06 -0500

    responsiveness in header, and addition of hbgr ico

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,22 +2,34 @@
   <div id="app">
     <div id="nav">
       <span id="name">Course Selection</span>
-      <img src="https://upload.wikimedia.org/wikipedia/en/e/e2/HSInfo1.jpeg" alt="hhs" width="100" hei>
+      <img id="hhs" src="https://upload.wikimedia.org/wikipedia/en/e/e2/HSInfo1.jpeg" alt="hhs" width="100" hei>
       <div id="links">
         <router-link to="/">Home</router-link>
         <router-link to="/">Courses</router-link> 
         <router-link to="/">Tags</router-link>
-        <router-link to="/" id="quiz">Quiz</router-link>
+        <router-link to="/Quiz.vue" id="quiz">Quiz</router-link>
       </div>
       <!-- <router-link to="/about">About</router-link> -->
+      <Hamburger/>
     </div>
     <router-view/>
   </div>
 </template>
 
+<script lang="ts">
+import Hamburger from "./components/sub/Hamburger.vue"
+
+export default {
+  name: 'Home',
+  components: {
+    Hamburger
+  }
+}
+</script>
+
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600&display=swap');
-
+/*#region*/
 :root {
   --red1: #EF233C;
   --red2: #D90429;
@@ -46,7 +58,7 @@
   
   transition: transform .3s;
 }
-
+/*#endregion*/
 * {
   font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: subpixel-antialiased;
@@ -66,9 +78,9 @@ body {
 }
 
 #nav {
-  padding: 30px;
+  padding: 15px;
   display: grid;
-  box-shadow: 0px 5px 43px 2px rgba(0,0,0,0.1);
+  height: calc(100px - 2 * 15px);
   z-index: 2;
 
   grid-template-rows: 1fr;
@@ -115,6 +127,33 @@ body {
 
   #name, a {
     place-self: center;
+  }
+
+  @media (min-width: 650px) {
+    #hamburger {
+      display:none;
+    }
+  }
+
+  @media (max-width: 765px) {
+    #hhs {
+      display: none;
+    }
+    & {
+      grid-template-columns: 200px 1fr auto;
+    }
+  }
+
+  @media (max-width: 650px) {
+    #hamburger {
+      grid-column: 3;
+      align-self: center;
+      justify-self: center;
+    }
+
+    #links {
+      display:none;
+    }
   }
 }
 </style>
