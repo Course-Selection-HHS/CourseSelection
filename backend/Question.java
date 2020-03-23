b772cf6616f281f13d302e3ce23714049209884f
Konrad Rozpadek <sbluek546@gmail.com>
2020-03-23 15:01:38 -0500

    Added getCourses

---
+++ backend/Question.java
@@ -0,0 +1,53 @@
+
+public class Question{
+
+    private String  questionType, answer;
+    private int questionID;
+    public Question(int questionID, String questionType, String answer){
+        this.questionID = questionID;
+        this.questionType = questionType;
+        this.answer = answer;
+    }
+  
+    /**
+     * @return the questionID
+     */
+    public int getQuestionID() {
+        return questionID;
+    }
+    
+    /**
+     * @return the answer
+     */
+    public String getAnswer() {
+        return answer;
+    }
+
+    /**
+     * @return the questionType
+     */
+    public String getQuestionType() {
+        return questionType;
+    }
+
+    /**
+     * @param questionID the questionID to set
+     */
+    public void setQuestionID(int questionID) {
+        this.questionID = questionID;
+    }
+
+    /**
+     * @param questionType the questionType to set
+     */
+    public void setQuestionType(String questionType) {
+        this.questionType = questionType;
+    }
+
+    /**
+     * @param answer the answer to set
+     */
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }    
+}
---
+++ backend/getCourses.java
@@ -0,0 +1,23 @@
+import java.io.FileWriter;
+import Server.*;
+import org.json.*;
+public class getCourses implements PostAction{
+    private Path postResponce;
+    public Path sendResponce(){
+        return postResponce;
+    } 
+    public void action(String request, Server server){
+        JSONObject json = new JSONObject(request);
+        JSONArray answers = json.getJSONArray("answers");
+        Question[] questions = new Question[answers.length()];
+        for(int i = 0; i < answers.length(); i++){
+            JSONObject answer = answers.getJSONObject(i);
+            Question question = new Question(answer.getInt("id"), answer.getString("type"), answer.getString("answer"));
+            questions[i] = question;
+        }
+        QuestionHandler courses = new QuestionHandler(questions);
+        
+
+        
+    }
+}
