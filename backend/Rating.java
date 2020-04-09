62c890394980e9a58cdd87a449f0d748653ef1cb
lementing-pixles <wright.j.tyler.24@gmail.com>
2020-04-08 23:41:42 -0500

    added class/file for rating question

---
+++ backend/Rating.java
@@ -0,0 +1,24 @@
+public class Rating{
+    public int score;
+    public String question;
+
+    public Rating(int sc, String quest) {
+        score = sc;
+        question = quest;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getTag() {
+        if (score > 5)
+            return question;
+        else
+            return "false";
+    }
+}
