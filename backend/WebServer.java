797aba7baec9f865927c4cbd00fb7ab577e661b5
Konrad Rozpadek <sbluek546@gmail.com>
2020-05-04 15:00:48 -0500

    Phase 1 Completed

--- backend/WebServer.java
+++ backend/WebServer.java
@@ -1,8 +1,8 @@
 import Server.*;
 public class WebServer {
     public static void main(String[] args){
-        Server server = new Server(8000, new Path[]{new Path("/getSurvey", "", "backend/pages/survey.json", Path.TXT ,true),new Path("/getTestResults", "", "backend/pages/testSurveyResults.json", Path.TXT ,true)}, new Post[]{new Post("/getCourses", "{}", "", Path.HTML, new getCourses())} );
-        System.out.println("Get Survey(GET): http://localhost:8000/getSurvey\nSend Survey Results(POST) http://localhost:8000/getCourses\nGet Fake Survey Results(GET) http://localhost:8000/getTestResults");
+        Server server = new Server(8000, new Path[]{new Path("/getSurvey", "", "backend/pages/survey.json", Path.TXT ,true),new Path("/getTestResults", "", "backend/pages/testSurveyResults.json", Path.TXT ,true), new Path("/debug", "", "backend/pages/debug.html", Path.HTML ,true)}, new Post[]{new Post("/getCourses", "{}", "", Path.HTML, new getCourses())} );
+        System.out.println("Get Survey(GET): http://localhost:8000/getSurvey\nSend Survey Results(POST) http://localhost:8000/getCourses\nGet Fake Survey Results(GET) http://localhost:8000/getTestResults\nSurvey Debugger Page http://localhost:8000/debug");
         server.start();
     }
 }
--- backend/getCourses.java
+++ backend/getCourses.java
@@ -1,22 +1,66 @@
 // import java.io.FileWriter;
 import Server.*;
 import org.json.*;
-public class getCourses implements PostAction{
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class getCourses implements PostAction {
     private Path postResponce;
-    public Path sendResponce(){
+
+    public Path sendResponce() {
         return postResponce;
-    } 
-    public void action(String request, Server server){
+    }
+
+    public void action(String request, Server server) {
         JSONObject json = new JSONObject(request);
         JSONArray answers = json.getJSONArray("answers");
         Question[] questions = new Question[answers.length()];
-        for(int i = 0; i < answers.length(); i++){
+        ArrayList<String> tags = new ArrayList<String>();
+        for (int i = 0; i < answers.length(); i++) {
             JSONObject answer = answers.getJSONObject(i);
             Question question = new Question(answer.getInt("id"), answer.getString("type"), answer.getString("answer"));
             questions[i] = question;
         }
-        //QuestionHandler courses = new QuestionHandler(questions);
-        
+        for (Question question : questions) {
+
+            if (question.getQuestionType().equals("checklist")) {
+                Checklist checklist = new Checklist(question.getQuestionID(), question.getAnswer());
+                String[] newTags;
+
+                try {
+                    newTags = checklist.getTags();
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    newTags = new String[]{};
+                }
+                for(String tag: newTags){
+                    tags.add(tag);
+                }
+            }
+        }
+        //convert to string[]
+        String[] tagsArr = new String[tags.size()];
+        for(int i = 0; i < tags.size(); i++){
+            tagsArr[i] = tags.get(i);
+        }
+        int[] allRating5 = new int[tagsArr.length];
+        for(int i = 0; i < allRating5.length; i++){
+            allRating5[i] = 5;
+        }
+        CourseProfile[] courses = P1FinalList.getClasses(tagsArr, allRating5);
+        System.out.println("courses.length");
+
+        JSONObject responceObj = new JSONObject();
+        JSONArray courseArr = new JSONArray();
+        System.out.println(courses.length);
+        for(CourseProfile course: courses){
+            courseArr.put(course.getName());
+            System.out.println(course.getName());
+        }
+        responceObj.put("courses", courseArr);
+        this.postResponce = new Path("/getCourses",responceObj.toString(), "", Path.TXT);
 
         
     }
