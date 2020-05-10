caea4ec427fd35caa71a9d5971d97c4eab8a6be7
Hamziniii <hamzagaziuddin04@gmail.com>
2020-05-10 17:23:57 -0500

    okey

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,5 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/CourseSelection/'
-      : '/'
-}
+// module.exports = {
+//     publicPath: process.env.NODE_ENV === 'production'
+//       ? '/CourseSelection/'
+//       : '/'
+// }
