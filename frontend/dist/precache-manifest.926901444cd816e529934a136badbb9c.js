083d94e149cbc2f1f1372fed101903c0f35ebdb6
Hamziniii <hamzagaziuddin04@gmail.com>
2020-05-10 17:41:53 -0500

    Add files via upload

---
+++ frontend/dist/precache-manifest.926901444cd816e529934a136badbb9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd93c7d03702503ae9e",
+    "url": "/CourseSelection/css/about.3a9399d2.css"
+  },
+  {
+    "revision": "add8959ee23a9044048c",
+    "url": "/CourseSelection/css/app.34c62a0c.css"
+  },
+  {
+    "revision": "a3f2dc6d852b3be7ebfafb13900de20a",
+    "url": "/CourseSelection/index.html"
+  },
+  {
+    "revision": "0bd93c7d03702503ae9e",
+    "url": "/CourseSelection/js/about.787dc08d.js"
+  },
+  {
+    "revision": "add8959ee23a9044048c",
+    "url": "/CourseSelection/js/app.4d8735c2.js"
+  },
+  {
+    "revision": "693f132a137ade79d605",
+    "url": "/CourseSelection/js/chunk-vendors.a3802a17.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/CourseSelection/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/CourseSelection/robots.txt"
+  }
+]);
