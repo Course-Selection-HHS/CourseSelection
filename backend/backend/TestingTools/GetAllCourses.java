21721113ac5f542e867bb82f1ffd4270b88c1632
Konrad Rozpadek <sbluek546@gmail.com>
2020-05-11 18:33:34 -0500

    Added new Database test case that checks all the courses in database

---
+++ backend/backend/TestingTools/GetAllCourses.java
@@ -0,0 +1,10 @@
+package backend.TestingTools;
+
+import backend.CourseData;
+
+public class GetAllCourses {
+    public static void main(String[] args){
+        System.out.println(new CourseData());
+        
+    }
+}
