441c407a02607aefd29f2dd12f4c9fa2bf6b31f4
Konrad Rozpadek <sbluek546@gmail.com>
2020-05-11 19:15:40 -0500

    Made Mongo Client connect on start

--- backend/backend/WebServer.java
+++ backend/backend/WebServer.java
@@ -13,6 +13,9 @@ public class WebServer {
         }else{
             port = new Integer(args[0]);
         }
+        //Open MongoDB
+        Database.COLLECTION_COURSES.count();
+        //start server
         Server server = new Server(port, new Path[]{new Path("/getSurvey", "", "./backend/Pages/survey.json", Path.TXT ,true),new Path("/getTestResults", "", "./backend/Pages/testSurveyResults.json", Path.TXT ,true), new Path("/debug", "", "./backend/Pages/debug.html", Path.HTML ,true),  new Path("/", "", "./backend/Pages/debug.html", Path.HTML ,true)}, new Post[]{new Post("/getCourses", "{}", "", Path.HTML, new getCourses())} );
         System.out.println("Get Survey(GET): http://localhost:8000/getSurvey\nSend Survey Results(POST) http://localhost:8000/getCourses\nGet Fake Survey Results(GET) http://localhost:8000/getTestResults\nSurvey Debugger Page http://localhost:8000/debug");
         server.start();
