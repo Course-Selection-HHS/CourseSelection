179551e830e44ce1962dfa0d091a094cecd60919
Konrad Rozpadek <sbluek546@gmail.com>
2020-05-14 13:04:44 -0500

    Compiled latest version

--- backend/build.bat
+++ backend/build.bat
@@ -1,1 +1,1 @@
-javac -cp ".\org-json.jar;.\mongo-java-driver-3.7.0-rc0.jar;.\google-api-client-1.30.9.jar" -d .\build .\backend\*.java .\backend\Server\*.java 
+javac -cp ".\org-json.jar;.\mongo-java-driver-3.7.0-rc0.jar;.\google-api-client-1.30.9.jar" -d .\build .\backend\*.java .\backend\Server\*.java .\backend\pages\*.java 
